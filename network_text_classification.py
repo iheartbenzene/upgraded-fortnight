87b9c71f0a6965eadf4e21c706141d7e588c3c32
Jevon Morris <iheartbenzene@gmail.com>
2019-05-13 11:44:59 -0700

    initialize text classification

--- network_text_classification.py
+++ network_text_classification.py
@@ -2,3 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
+data = keras.datasets.imdb
+
+(train_data, train_label), (test_data, test_label) = data.load_data(num_words=80000)
+
