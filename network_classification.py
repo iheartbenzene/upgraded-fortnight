9ab3464675af933570aaa240504e9272a2d653e2
Jevon Morris <iheartbenzene@gmail.com>
2019-05-13 11:16:29 -0700

    initialize mnist fashion data

---
+++ network_classification.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = keras.datasets.fashion_mnist
+
+(train_image, train_labels), (test_image, test_labels) = data.load_data()
+
+class_names = ['t-shirt/top', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+
+train_image = train_image/255.0
+test_image = test_image/255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
