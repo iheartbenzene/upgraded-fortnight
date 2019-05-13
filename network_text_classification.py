62ff4a2e4df456d0a79a14ce93250a3d9bc3a953
Jevon Morris <iheartbenzene@gmail.com>
2019-05-13 12:12:31 -0700

    initialize model layers

--- network_text_classification.py
+++ network_text_classification.py
@@ -30,3 +30,5 @@ model.add(keras.layers.Embedding(10000, 16))
 model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16, activation="relu"))
 model.add(keras.layers.Dense(1, activation="sigmoid"))
+
+model.summary()
