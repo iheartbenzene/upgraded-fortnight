bc3c28325d3f8b4ef5c07bae0ea4fa748e922723
Jevon Morris <iheartbenzene@gmail.com>
2019-05-13 11:18:25 -0700

    initialize model

--- network_classification.py
+++ network_classification.py
@@ -16,5 +16,7 @@ model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation="relu"),
     keras.layers.Dense(10, activation="softmax")
-    
 ])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
