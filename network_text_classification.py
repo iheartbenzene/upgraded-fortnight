b50ceeaf611281a91ac4b701b46618d23fa2717e
Jevon Morris <iheartbenzene@gmail.com>
2019-05-13 11:51:10 -0700

    initialize vocabulary index

--- network_text_classification.py
+++ network_text_classification.py
@@ -6,3 +6,10 @@ data = keras.datasets.imdb
 
 (train_data, train_label), (test_data, test_label) = data.load_data(num_words=80000)
 
+vocabulary = imdb.get_word_index()
+
+vocabulary = {key:(value = 3) for key, value in vocabulary.items()}
+vocabulary["<PAD>"] = 0
+vocabulary["<START>"] = 1
+vocabulary["<UNK>"] = 2
+vocabulary["<UNUSED>"] = 3
