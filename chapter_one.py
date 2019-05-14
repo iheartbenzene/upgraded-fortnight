bad73e36829eee3831dfd9635043197f78993dac
Jevon Morris <iheartbenzene@gmail.com>
2019-05-14 10:00:28 -0700

    merge with statisticas

---
+++ chapter_one.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+import scipy
+from scipy.stats import fisher_exact
+from scipy.stats import chisquare
+
+from scipy.stats import linregress
+from scipy.stats import pearsonr, spearmanr
+
+from scipy.stats import ttest_1samp
+from numpy import fmax
+from scipy.stats import bartlett
+
+from scipy.stats import mannwhitneyu, wilcoxon
+
+from scipy.stats import f_oneway
+
+
+def regression_analysis():
+    pass
+
+def pearson_r():
+    pass
+
+def spearman_rank_correlation():
+    pass
+
+def one_sample():
+    pass
+
+def two_sample():
+    pass
+
+def fmax():
+    pass
+
+def bartlett():
+    pass
+
+def chi_squared():
+    pass
+
+def fischer_exact():
+    pass
+
+def students_t():
+    pass
+
+def mann_whitney_u():
+    pass
+
+def wilcoxon():
+    pass
+
+def one_way_ANOVA():
+    pass
+
+def kruskall_wallace():
+    pass
