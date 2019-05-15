af1292d9a82e616902984febeb2e3466e7fc7beb
Jevon Morris <iheartbenzene@gmail.com>
2019-05-15 12:23:53 -0700

    initialize fibonnaci

--- bayesian.r
+++ bayesian.r
@@ -7,16 +7,34 @@ multiply <- function(x, y) {
 }
 
 factorial <- function(x) {
-    if (x <= 1) {
-       print(1)
+    if (x == 0) {
+       return (1)
     }
     else {
-       x * factorial(x-1)
+       return(x * factorial(x-1))
     }
+    end
+}
+
+fibonacci <- function(x) {
+    if (x<=1) {
+       return (1)
+    }
+    else if (x>1) {
+       return(fibonacci(x-1) + fibonacci(x-2))
+    }
+    end
 }
 
 print(add(1, 2))
 print(add(1, -2))
 print(multiply(2, 3))
 print(multiply(2, 3/4))
+print("Factorial start")
 print(factorial(3))
+print(factorial(1))
+print("Factorial end")
+print("Fibonacci start")
+print(fibonacci(5))
+print(fibonacci(1))
+print('Fibonacci end')
