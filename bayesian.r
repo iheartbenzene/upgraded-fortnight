6db5779c3fd990773cd50a2a91a3b25799146402
Jevon Morris <iheartbenzene@gmail.com>
2019-05-15 12:02:18 -0700

    initialize factorial

--- bayesian.r
+++ bayesian.r
@@ -6,8 +6,17 @@ multiply <- function(x, y) {
     x * y
 }
 
+factorial <- function(x) {
+    if (x <= 1) {
+       print(1)
+    }
+    else {
+       x * factorial(x-1)
+    }
+}
 
 print(add(1, 2))
 print(add(1, -2))
 print(multiply(2, 3))
 print(multiply(2, 3/4))
+print(factorial(3))
