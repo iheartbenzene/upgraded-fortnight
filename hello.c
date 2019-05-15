96997298844bbc5033b6bc919ff2425054a0f5c4
Jevon Morris <iheartbenzene@gmail.com>
2019-05-15 14:53:52 -0700

    messing around in C

--- hello.c
+++ hello.c
@@ -5,20 +5,20 @@ int displayPrimeNumbers();
 int main(){
     printf("This is a test.");
 
-    int r1, r2, k, flag;
-    printf("Enter two whole numbers:");
-    scanf("%d %d", &r1, &r2);
-    printf("Here you go.");
-    printf("Primes between %d and %d are as follows: ", &r1, &r2);
+    // int r1, r2, k, flag;
+    // printf("Enter two whole numbers:");
+    // scanf("%d %d", &r1, &r2);
+    // printf("Here you go.");
+    // printf("Primes between %d and %d are as follows: ", &r1, &r2);
 
-    for(k=r1+1; k<r2; ++k){
-        flag = displayPrimeNumbers(k);
+    // for(k=r1+1; k<r2; ++k){
+    //     flag = displayPrimeNumbers(k);
 
-        if(flag == 1){
-            printf("%d", &k);
-        }
-    }
-    return 0;
+    //     if(flag == 1){
+    //         printf("%d", &k);
+    //     }
+    // }
+    // return 0;
 
 }
 
